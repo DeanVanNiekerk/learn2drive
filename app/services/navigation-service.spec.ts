5c2433780bfad16968f4e7b5ace48e8daf82d62b
Dean van Niekerk <Dean van Niekerk>
2016-07-26 14:44:10 +0200

    Added first test

---
+++ app/services/navigation-service.spec.ts
@@ -0,0 +1,12 @@
+import {beforeEachProviders, it, describe, expect, inject} from '@angular/core/testing';
+import {NavigationService} from './navigation-service';
+ 
+describe('first test', () => {
+ 
+    it('should pass!', () => {
+ 
+        expect(true).toBeTruthy();
+ 
+    });
+ 
+});
